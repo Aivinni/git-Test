36cf913d11cc416585f4cdd414aae9f7c922af1f
aivin1 <aivinni1219@gmail.com>
2023-12-15 10:46:27 -0500

    Add

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("no bye");
         //weigfiuweuagbfiawegifbsilafbsjdhBkjbnsdK
         System.out.println("Hello World * 2");
+
+
+        System.out.println("Hello World");
     }
 }
